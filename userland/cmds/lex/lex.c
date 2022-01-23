f8612aa07f9dfc719863b489998500bfc4667c49
Hellwig Geisse <hellwig.geisse@mni.thm.de>
2022-01-23 10:37:36 +0100

    add fake implementations of lex and yacc

---
+++ userland/cmds/lex/lex.c
@@ -0,0 +1,12 @@
+/*
+ *
+ */
+
+
+#include <stdio.h>
+
+
+int main(int argc, char *argv[]) {
+  printf("This 'lex' is a fake, sorry.\n");
+  return 0;
+}
---
+++ userland/cmds/yacc/yacc.c
@@ -0,0 +1,12 @@
+/*
+ *
+ */
+
+
+#include <stdio.h>
+
+
+int main(int argc, char *argv[]) {
+  printf("This 'yacc' is a fake, sorry.\n");
+  return 0;
+}
