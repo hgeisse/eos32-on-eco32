2c4bf29307c03385cc8c26d40566303fe8ca3385
Sebastian Walter <sebastian.walter@mni.thm.de>
2022-02-19 15:07:08 +0100

    implemented strtod

--- userland/lib/libsrc/stdlib.c
+++ userland/lib/libsrc/stdlib.c
@@ -53,10 +53,52 @@ long atol(const char *s) {
  */
 
 double strtod (const char* str, char** endptr) {
-  /* !!!!! */
-  return 0.0; /*dirty workaround */
-  assert(1 == 0);
-  return 0.0;
+    double result1 = 0.0; /* left site of dot */
+    double result2 = 0.0; /* right site of dot */
+    double* cur;
+    char sign = +1;
+    int rsc = 0; /* right site counter */
+    int quot = 1; /* quotient */
+
+    cur = &result1;
+    if( ! str) goto output;
+    while(*str) {
+        switch(*str) {
+            case ' ':  case '\n': case '\t': 
+            case '\v': case '\f': case '\r':
+                if(result1 != 0.0 || result2 != 0.0) goto output; 
+                /* just ignore whitespaces */
+                break;
+            case '+':
+                if(result1 != 0.0 || result2 != 0.0) goto output;
+                sign = +1;
+                break;
+            case '-':
+                if(result1 != 0.0 || result2 != 0.0) goto output; 
+                sign = -1;
+                break;
+            case '.':
+                cur = &result2;
+                rsc = 0;
+                break;
+            case '0': case '1': case '2': case '3': case '4':
+            case '5': case '6': case '7': case '8': case '9':
+                *cur *= 10;
+                *cur += (*str) - '0';
+                rsc++;
+                break;
+            default:
+                goto output;
+        }
+        str++;
+    }
+    output:
+    if(endptr) *endptr = (char*) str;
+    /* move result2 to the right site of the dot (mathematically, if any) */
+    while(rsc--) quot *= 10;
+    if(result2 != 0.0) result2 /= quot;
+    
+    return sign * (result1 + result2);
 }
 
 /*
