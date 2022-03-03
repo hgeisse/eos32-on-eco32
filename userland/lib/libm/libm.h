bdeefac049b2f570403b98642620fea8c7a9af13
Beate Franken <beate.franken@mni.thm.de>
2022-03-03 12:12:58 +0100

    Cleanup of internal libm header file

--- userland/lib/libm/libm.h
+++ userland/lib/libm/libm.h
@@ -1,45 +1,32 @@
 #ifndef __LIBM_H_
 #define __LIBM_H_
-
-/* bfranken DEBUG/DOC: 
-  in musl this is implemented as a macro using a union:
-  #define GET_FLOAT_WORD(i,d) \
-  do {                        \
+  
+#define GET_FLOAT_WORD(i,d) \
+  {                           \
     union {                   \
       float value;            \
-      unsigned bits;          \
+      unsigned int bits;      \
     } __u;                    \
-    _u.value = (d);           \
+    __u.value = (d);          \
     (i) = __u.bits;           \
-  } while (0)
+  }
 
-  This is not possible for our implementation, because C89 only
-  allows local Variables at the beginning of a scope.
-  TODO bfranken I could try to wrap the macro in { }
-
-  As it is, this implementation requires __asuint from asuint.c
-  */
-#define GET_FLOAT_WORD(w,d)                       \
-do {                                              \
-  (w) = __asuint(d);                              \
-} while (0)
-
-#define SET_FLOAT_WORD(d,w)                       \
-do {                                              \
-  (d) = __asfloat(w);                               \
-} while (0)
+#define SET_FLOAT_WORD(d,i)                       \
+{                                                 \
+  union {                                         \
+    float value;                                  \
+    unsigned int bits;                            \
+  } __u;                                          \
+  __u.bits = (i);                                \
+  (d) = __u.value;                                 \
+}
 
 
 /* prototypes for internal math library functions */
 
-/*
-float __asfloat(unsigned int i);
-unsigned int __asuint(float f);
-int __ieee754_rem_pio2f(float x, float *y);
-int __kernel_rem_pio2f(float *x, float *y, int e0, int nx, int prec, const int *ipio2);
 float __kernel_cosf(float x, float y);
 float __kernel_sinf(float x, float y, int iy);
-*/
-float sin(float x);
+int __ieee754_rem_pio2f(float x, float *y);
+int __kernel_rem_pio2f(float *x, float *y, int e0, int nx, int prec, const int *ipio2);
 
 #endif /* __LIBM_H_ */
