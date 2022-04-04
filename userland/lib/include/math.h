03cb37b1c3a807459d5f344824ca7583001c187d
Beate Franken <beate.franken@mni.thm.de>
2022-04-04 12:30:34 +0200

    Add ceil implementation and tests

--- userland/lib/include/math.h
+++ userland/lib/include/math.h
@@ -44,6 +44,7 @@ float acos(float x);
 float asin(float x);
 float atan(float x);
 float atan2(float y, float x);
+float ceil(float x);
 float copysign(float x, float y);
 float cos(float x);
 float cosh(float x);
