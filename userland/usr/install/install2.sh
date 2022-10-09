f6a076fa36824f2bae10ee2e675a92b480f5d45e
Sebastian Walter <sebastian.walter@mni.thm.de>
2022-10-09 18:37:46 +0200

    some more userland cmds with sources, modified install scripts slightly

--- userland/usr/install/install2.sh
+++ userland/usr/install/install2.sh
@@ -1,32 +1,20 @@
-: PLEASE MODIFY THE FOLLOWING 4 LINES
-PART_ROOT=4
-SIZE_ROOT=64000
 ROOTFS=/usr/install/rootfs
 
-: INTERNAL
-DISK_FILE=/dev/idedisk
-DISK_MAJOR=0
-
-echo "## Step 2.1: Create devices under /dev for the new partitions..."
-mknod "${DISK_FILE}p${PART_ROOT}" b $DISK_MAJOR $PART_ROOT
-
-echo "## Step 2.2: Compile the partition bootstrap..."
-cd /usr/src/bin/mkpboot; make
-cd /usr/install
-
-echo "## Step 2.3: Format root partition..."
-mkfs "${DISK_FILE}p${PART_ROOT}" ./root.fsys
-
-echo "## Step 2.4: Mount new root partition..."
-mkdir ./rootfs
-mount "${DISK_FILE}p${PART_ROOT}" ./rootfs
-
-echo "## Step 2.5: Compile kernel sources..."
+echo "## Step 2.0: Compile kernel sources..."
 cd /usr/src/kernel; make
-cd /usr/install
 
-echo "## Step 2.6: Copy kernel binaries to rootfs..."
-mkdir "${ROOTFS}/boot"
-cp /usr/src/kernel/src/eos32.map "${ROOTFS}/boot/eos32.map"
-cp /usr/src/kernel/src/eos32.bin "${ROOTFS}/boot/eos32.bin"
-sync
+echo "## Step 2.1: Compile the init binary"
+cd /usr/src/bin/init; make
+
+echo "## Step 2.2: Compile getty"
+cd /usr/src/bin/getty; make
+
+echo "## Step 2.3: Compile userland commands"
+echo " sh..."
+cd /usr/src/bin/sh; make
+echo " ls..."
+cd /usr/src/bin/ls; make
+echo " echo..."
+cd /usr/src/bin/echo; make
+echo " cat..."
+cd /usr/src/bin/cat; make
--- userland/usr/install/install3.sh
+++ userland/usr/install/install3.sh
@@ -1,26 +1,22 @@
+: PLEASE MODIFY THE FOLLOWING 4 LINES
+PART_ROOT=4
+SIZE_ROOT=64000
 ROOTFS=/usr/install/rootfs
 
-echo "This script prepares the minimal needed userland."
-echo "## Step 3.1: Compile the init binary"
-cd /usr/src/bin/init; make
+: INTERNAL
+DISK_FILE=/dev/idedisk
+DISK_MAJOR=0
+
+echo "## Step 3.1: Create devices under /dev for the new partitions..."
+mknod "${DISK_FILE}p${PART_ROOT}" b $DISK_MAJOR $PART_ROOT
+
+echo "## Step 3.2: Compile the partition bootstrap..."
+cd /usr/src/bin/mkpboot; make
 cd /usr/install
 
-echo "## Step 3.2: Compile getty"
-cd /usr/src/bin/getty; make
-cd /usr/install
+echo "## Step 3.3: Create root partition..."
+mkfs "${DISK_FILE}p${PART_ROOT}" ./root.fsys
 
-echo "## Step 3.3: Compile the shell (sh)"
-cd /usr/src/bin/sh; make
-cd /usr/install
-
-echo "## Step 3.99: Copy binaries to the new partition"
-mkdir "${ROOTFS}/etc"
-cp /usr/src/bin/init/init "${ROOTFS}/etc/init"
-cp /usr/src/bin/getty/getty "${ROOTFS}/etc/getty"
-mkdir "${ROOTFS}/bin"
-cp /usr/src/bin/sh/sh "${ROOTFS}/bin/sh"
-sync
-
-echo "## Step 3.100: Copy config files to the new partition"
-cp /etc/rc "${ROOTFS}/etc/rc"
-cp /etc/ttys "${ROOTFS}/etc/ttys"
+echo "## Step 3.4: Mount new root partition..."
+mkdir ./rootfs
+mount "${DISK_FILE}p${PART_ROOT}" ./rootfs
